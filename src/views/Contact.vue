f6db6386bcc336041a54b1d54cf8300b85ef74ab
Rakesh Jangid <rakesh@jangid.co.in>
2024-04-10 10:35:50 +0530

    typos corrected

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,19 @@
 <template>
-<div id="contact">
+<div class="flex">
+  <div class="p-6 w-5/12">
+    <img :src="geotag" class="p-6 m-6">
+  </div>
+  <div id="contact">
     <h2 class="text-3xl mx-14 pt-16 pb-0 font-serif border-b-2 border-orange-500 w-10/12">Contact Us</h2>
     <p class="text-xl w-10/12 text-left mx-16 pt-16">Principal</p>
     <p class="text-xl w-10/12 text-left mx-16 pt-5 pb-2 font-bold text-blue-900">Sharvani Jhajharia Institute of Education</p>
-    <p class="text-xl w-10/12 text-left mx-16 pb-2">Mankro Road, Singhana - 333516, Jhunjhunu, Rajasthan</p>
+    <p class="text-xl w-10/12 text-left mx-16 pb-12">Mankro Road, Singhana - 333516, Jhunjhunu, Rajasthan</p>
+    <p class="text-xl w-10/12 text-left mx-16 pb-12">Lat-Lon: 28.097291, 75.816395</p>
     <p class="text-xl w-10/12 text-left mx-16 pb-24">Phone: 01593-222697</p>
     <p class="text-xl w-10/12 text-left mx-16 pb-24">Email Id: <a href="mailto:sharvaniji@gmail.com">sharvaniji@gmail.com</a></p>
   </div>
+</div>
 </template>
+<script setup>
+import geotag from '../assets/geotag.jpg'
+</script>
--- src/views/Documents.vue
+++ src/views/Documents.vue
@@ -3,15 +3,15 @@
 <div class="container w-full flex mx-auto p-6 pl-0">
   <p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="committee" target="_blank"><img :src="pdf" class="w-14" /><span>Executive Committee</span></a></p>
   <p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="land" target="_blank"><img :src="pdf" class="w-14" /><span>Land Transformation</span></a></p>
-<p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="ncte" target="_blank"><img :src="pdf" class="w-14" /><span>NCTE Recognition Letter</span></a></p>
+<p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="ncte" target="_blank"><img :src="pdf" class="w-14" /><span>State NOC</span></a></p>
 <p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="extention" target="_blank"><img :src="pdf" class="w-14" /><span>DCE Extention Letter</span></a></p>
-<p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="affiliation" target="_blank"><img :src="pdf" class="w-14" /><span>PDU University Affiliation Letter</span></a></p>
+<p class="align-left w-1/3 text-blue-800 hover:text-cyan-600 hover:bg-yellow-200 p-5 rounded-lg m-5"><a :href="affiliation" target="_blank"><img :src="pdf" class="w-14" /><span>PDU Affiliation Extention Letter</span></a></p>
 </div>
 </template>
 <script setup>
-import ncte from '../assets/nct-approval.pdf'
-import extention from '../assets/extention-letter.pdf'
-import affiliation from '../assets/university-affiliation.jpg'
+import ncte from '../assets/STATE NOC.pdf'
+import extention from '../assets/dce-extention.pdf'
+import affiliation from '../assets/pdu-extention.pdf'
 import committee from '../assets/executive-committee.pdf'
 import land from '../assets/land.pdf'
 import pdf from '../assets/pdf17.svg'
